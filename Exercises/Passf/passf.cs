b50cf589fae50d0830025982015d4897295fcfa4
Jens Reersted Larsen <jensrlarsen@Jenss-MacBook-Air.local>
2022-02-22 13:10:54 +0100

    Ajouring all exexercises to date  and adding GenericList Homework

--- Exercises/Passf/passf.cs
+++ Exercises/Passf/passf.cs
@@ -3,13 +3,15 @@ using static System.Console;
 using static System.Math; 
 
 public class passf{
-	private static void make_table(Func<double,double> f, double a, double b, double dx){
-		while (a < b){
+	public static void make_table(Func<double,double> f, double a, double b, double dx){
+		while (a <= b){
 			WriteLine($"x = {a}, sin(x) = {f(a)}");
 			a += dx;
-		} 
+		}
+		/*
 		while (a >= b){
 			WriteLine($"x = {b}, sin(x) = {f(b)}");
 		}
+		*/
 	}
 }
---
+++ Homeworks/GenericList/B/genlist.cs
@@ -0,0 +1,25 @@
+using static System.Console;
+
+public class genlist<T>{
+	public T[] data;
+	public int size=0,capacity=2;
+	public genlist(){ data = new T[capacity]; }
+	public void push(T item){ /* add item to list */
+		if(size==capacity){
+			T[] newdata = new T[capacity*=2];
+			for(int i=0;i<size;i++)
+				newdata[i]=data[i];
+				data=newdata;
+			}
+		data[size]=item;
+		size++;
+		WriteLine($"Size of array: {size}");
+		WriteLine($"Capacity of array: {capacity}");
+		}
+	/*
+	public void remove(int i){
+		
+	}
+	*/
+}
+
